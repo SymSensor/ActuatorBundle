e9b930701c9a2821b4ee8b95d270870d0e5b5dfa
kreemer <kreemer@me.com>
2023-08-07 11:32:13 +0200

    Structure of bundle created

---
+++ src/SymSensorActuatorBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the symsensor/actuator package.
+ *
+ * (c) Kevin Studer <kreemer@me.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymSensor\ActuatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+final class SymSensorActuatorBundle extends AbstractBundle
+{
+}
